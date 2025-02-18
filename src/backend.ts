bda674717121a0a4c8e99256bbefa39f513539cc
clemence-l <clemence.lauga@hotmail.com>
2025-02-18 13:47:12 +0100

    .env.developpement

--- src/backend.ts
+++ src/backend.ts
@@ -1,4 +1,9 @@
-import Pocketbase from 'pocketbase'
-import type { TypedPocketBase } from './pocketbase-typegen'
+import Pocketbase from "pocketbase";
+import type { TypedPocketBase } from "./pocketbase-typegen";
 
-export const pb = new Pocketbase("http://127.0.0.1:8090/") as TypedPocketBase;
+// export const pb = new Pocketbase("http://127.0.0.1:8090/") as TypedPocketBase;
+
+export const pb = new Pocketbase(
+  import.meta.env.VITE_URL_POCKETBASE
+) as TypedPocketBase;
+pb.autoCancellation(false);
