5571d58b2a91a500f04c2c24248d3cc02ab6fa27
clemence-l <clemence.lauga@hotmail.com>
2025-02-20 10:30:38 +0100

    composants

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -5,7 +5,7 @@ import { defineMiddleware } from "astro/middleware";
 
 export const onRequest = defineMiddleware(
   async ({ locals, request }, next: () => any) => {
-    locals.pb = new PocketBase(import.meta.env.VITE_URL_POCKETBASE);
+    locals.pb = new PocketBase("http://127.0.0.1:8090/");
 
     // load the store data from the request cookie string
     locals.pb.authStore.loadFromCookie(request.headers.get("cookie") || "");
